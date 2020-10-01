9d23c09d172f8f609acb9f994cb3ab8c3c0deb5a
Janaina Oliveira <janainaon1@gmail.com>
2020-09-30 21:54:29 -0300

    add features

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,15 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const sum = products.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0,
+    );
+    return formatValue(sum);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    return products.reduce((total, item) => total + item.quantity, 0);
   }, [products]);
 
   return (
